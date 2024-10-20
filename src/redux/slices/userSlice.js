8ac4c83ca0895fa33b665a714238f0354b4f981d
PalsAnalytix <palsanalytix.dev@gmail.com>
2024-10-20 21:31:16 +0530

    debuggin

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,33 +1,39 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // Async thunk for fetching user data
 export const fetchUserData = createAsyncThunk(
-  'user/fetchUserData',
+  "user/fetchUserData",
   async (userId, thunkAPI) => {
     try {
       const response = await axios.get(`${BASE_URL}/user/${userId}`);
       console.log(response);
       if (!response.status == 200) {
-        throw new Error('Failed to fetch user data');
+        throw new Error("Failed to fetch user data");
       }
       const userData = await response.data;
       console.log(userData);
 
-      console.log("bulk request se upr")
+      console.log("bulk request se upr");
 
       // Fetch the question details in bulk using the question IDs from user data
-      if (userData.attemptedQuestions && userData.attemptedQuestions.length > 0) {
-        console.log("bulk request k andr")
-        const questionDetails = await thunkAPI.dispatch(fetchBulkQuestionDetails(userData.attemptedQuestions)).unwrap();
-
-        console.log("questionDetails");
-        console.log(questionDetails);
+      if (
+        userData.attemptedQuestions &&
+        userData.attemptedQuestions.length > 0
+      ) {
+        console.log("Attempting to fetch question details");
+        const questionDetails = await thunkAPI
+          .dispatch(fetchBulkQuestionDetails(userData.attemptedQuestions))
+          .unwrap();
+        console.log(
+          "Fetch successful, question details retrieved:",
+          questionDetails
+        );
         userData.questionDetails = questionDetails;
         userData.questionStats = calculateQuestionStats(questionDetails);
-        console.log("bulk req m")
+        console.log("bulk req m");
       }
 
       console.log("userData fetched");
@@ -41,13 +47,15 @@ export const fetchUserData = createAsyncThunk(
 
 // Async thunk for fetching question details in bulk
 export const fetchBulkQuestionDetails = createAsyncThunk(
-  'user/fetchBulkQuestionDetails',
+  "user/fetchBulkQuestionDetails",
   async (questionIds, thunkAPI) => {
     try {
       console.log(questionIds);
-      const response = await axios.post(`${BASE_URL}/getQuestionsByIds`,{ids : questionIds});
+      const response = await axios.post(`${BASE_URL}/getQuestionsByIds`, {
+        ids: questionIds,
+      });
       if (!response.statusText) {
-        throw new Error('Failed to fetch question details');
+        throw new Error("Failed to fetch question details");
       }
       console.log(response);
       console.log("bulkQuestion fetched");
@@ -85,8 +93,11 @@ export const fetchBulkQuestionDetails = createAsyncThunk(
 // };
 
 export const updateUserWhatsAppDetails = createAsyncThunk(
-  'user/updateUserWhatsAppDetails',
-  async ({ phoneNo, currentChapterForWhatsapp, currentCourseForWhatsapp }, { getState }) => {
+  "user/updateUserWhatsAppDetails",
+  async (
+    { phoneNo, currentChapterForWhatsapp, currentCourseForWhatsapp },
+    { getState }
+  ) => {
     const { auth0ID } = getState().userDetails;
     const response = await axios.put(`${BASE_URL}/user/${auth0ID}/whatsapp`, {
       phoneNo,
@@ -94,10 +105,11 @@ export const updateUserWhatsAppDetails = createAsyncThunk(
       currentCourseForWhatsapp,
     });
 
-    const message = "Hello from PalsAnalytix, Thank you for choosing us and we will help yu in every way possible to achieve your dreams."
+    const message =
+      "Hello from PalsAnalytix, Thank you for choosing us and we will help yu in every way possible to achieve your dreams.";
     const accessToken = import.meta.env.VITE_WHATSAPP_ACCESS_TOKEN;
-    if(response){
-      sendWhatsAppMessage(phoneNo, message,accessToken);
+    if (response) {
+      sendWhatsAppMessage(phoneNo, message, accessToken);
     }
     return response.data;
   }
@@ -109,13 +121,13 @@ const calculateQuestionStats = (questions) => {
 
   questions.forEach((question) => {
     questionStats.Total++;
-    if (question.courses.includes('CFA')) {
+    if (question.courses.includes("CFA")) {
       questionStats.CFA++;
     }
-    if (question.courses.includes('FRM')) {
+    if (question.courses.includes("FRM")) {
       questionStats.FRM++;
     }
-    if (question.courses.includes('SCR')) {
+    if (question.courses.includes("SCR")) {
       questionStats.SCR++;
     }
     if (question.isCorrect) {
@@ -128,28 +140,28 @@ const calculateQuestionStats = (questions) => {
 };
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     _id: null,
-    name: '',
-    email: '',
-    auth0ID: '',
-    picture: '',
-    phoneNo: '',
+    name: "",
+    email: "",
+    auth0ID: "",
+    picture: "",
+    phoneNo: "",
     attemptedQuestions: [],
-    questionDetails : [],
+    questionDetails: [],
     attemptedTests: [],
-    subscriptionId: '',
+    subscriptionId: "",
     purchaseDate: null,
     expiryDate: null,
-    paymentId: '',
+    paymentId: "",
     amountPaid: 0,
     __v: 0,
     isLoggedIn: false,
     loading: false,
     error: null,
-    currentChapterForWhatsapp : null,
-    currentCourseForWhatsapp : null,
+    currentChapterForWhatsapp: null,
+    currentCourseForWhatsapp: null,
     questionStats: { CFA: 0, FRM: 0, SCR: 0, Total: 0, Right: 0 },
   },
   reducers: {
